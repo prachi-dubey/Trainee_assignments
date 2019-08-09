693554ee17968c06ffaac8efd8a424081bf327f3
prachi-dubey <prachi@newput.com>
2019-08-09 10:33:42 +0530

    table using javascript DOM

--- js_practice_assignment/table/tableJs.js
+++ js_practice_assignment/table/tableJs.js
@@ -1,62 +1,132 @@
-var person = {
-  id: "",
-  fname: "",
-  lname: "",
-  age: "",
-  profile: ""
-};
-var rowNumber = 1;
+var personRecords = [];
+var getUserData = document.getElementById("insert-data");
 
-function showInputRow() {
-  var x = document.getElementById("insert-data");
-  if (x.style.display === "none") {
-    x.style.display = "block";
+function getRowData(id, isEdit, editdata) {
+  var data = null;
+  var trHtml = "";
+  if (id) {
+    data = personRecords[parseInt(id-1)];
+  }
+  if (!data) {
+    id = personRecords.length + 1;
+    var row = document.getElementById("table-row");
+    var rowElem = document.createElement('tr');
+    rowElem.setAttribute("id", "row-" + id);
+    row.appendChild(rowElem);
+  }
+  var tmp = document.getElementById("row-" + id);
+  trHtml = '<td>' + id + '</td>';
+  if (isEdit) {
+    trHtml += '<td>'+ (editdata.fname ? editdata.fname : "n/a") + '</td>';
+    trHtml += '<td>'+ (editdata.lname ? editdata.lname : "n/a") + '</td>';
+    trHtml += '<td>' + (editdata.age ? editdata.age : "n/a") + '</td>';
+    trHtml += '<td>' + (editdata.profile ? editdata.profile : "n/a") + '</td>';
+    trHtml += '<td> <button onclick="getRowData('+ id +')" class="btn btn-success"> EDIT </button> </td>';
   } else {
-    x.style.display = "none";
+    trHtml += '<td> <input id="fname" class="form-control" type="text" placeholder="firstName" value="' + (data ? data.fname: '') + '"> </td>';
+    trHtml += '<td> <input id="lname" class="form-control" type="text" placeholder="lastName" value="' + (data ? data.lname: '') + '"> </td>';
+    trHtml += '<td> <input id="age" class="form-control" type="text" placeholder="age" value="' + (data ? data.age: '') + '"> </td>';
+    trHtml += '<td> <input id="profile" class="form-control" type="text" placeholder="profile"  value="' + (data ? data.profile: '') + '"></td>';
+    trHtml += '<td> <button onclick="storeEditData(' + id + ')" class="btn btn-success"> SAVE </button> </td>';
+  }
+  tmp.innerHTML = trHtml;
+}
+
+function storeEditData(id) {
+   var person = personRecords[(id-1)];
+   var fname = document.getElementById("fname").value;
+   var lname = document.getElementById("lname").value;
+   var age = document.getElementById("age").value;
+   var profile = document.getElementById("profile").value;
+   if(person) {
+     person.fname = fname;
+     person.lname = lname;
+     person.age = age;
+     person.profile= profile;
+   } else {
+     person = {
+       id: id,
+       fname: fname,
+       lname: lname,
+       age: age,
+       profile: profile
+     };
+     personRecords.push(person);
+   }
+   getRowData(id, true, person);
+
+   console.log(personRecords);
+}
+
+
+/*
+function showInputRow() {
+  getUserData = document.getElementById("insert-data");
+  if (getUserData.style.display === "none") {
+    getUserData.style.display = "table-row";
+    document.getElementById("id-no").innerHTML = (idVAlue);
+  } else {
+    getUserData.style.display = "none";
   }
 }
 
 function getRowData() {
-
-  person.id = document.getElementById("idNo").value;
-  person.fname = document.getElementById("firstName").value;
-  person.lname = document.getElementById("lastName").value;
+  var person = {
+    id: idVAlue,
+    fname: "",
+    lname: "",
+    age: "",
+    profile: "",
+  };
+  person.fname = document.getElementById("first-name").value;
+  person.lname = document.getElementById("last-name").value;
   person.age = document.getElementById("age").value;
   person.profile = document.getElementById("profile").value;
+
+  if( person.fname == '' && person.lname == '' && person.age == '' && person.profile == '') {
+    document.getElementById("error").innerHTML = "Enter at least one parameter!";
+    return false;
+  } else {
+    document.getElementById("error").innerHTML = "";
+  }
+
   insertRowData(person);
-  document.getElementById("idNo").value = null;
-  document.getElementById("firstName").value = null;
-  document.getElementById("lastName").value = null;
+  document.getElementById("first-name").value = null;
+  document.getElementById("last-name").value = null;
   document.getElementById("age").value = null;
   document.getElementById("profile").value = null;
 }
 
-
-function insertRowData() {
-  // console.log("inserting row in your table");
-  // console.log("my row numb is " +rowNumber);
-  var table = document.getElementById("wrapper");
+function insertRowData(personData) {
   var row = table.insertRow(rowNumber);
-  var cell1 = row.insertCell(0);
-  var cell2 = row.insertCell(1);
-  var cell3 = row.insertCell(2);
-  var cell4 = row.insertCell(3);
-  var cell5 = row.insertCell(4);
-  cell1.innerHTML= person.id;
-  cell2.innerHTML = person.fname;
-  cell3.innerHTML= person.lname;
-  cell4.innerHTML = person.age;
-  cell5.innerHTML= person.profile;
+  var idCell = row.insertCell(0);
+  var fnameCell = row.insertCell(1);
+  var lnameCell = row.insertCell(2);
+  var ageCell = row.insertCell(3);
+  var profileCell = row.insertCell(4);
+  var actionCell = row.insertCell(5);
 
-  document.getElementById("insert-data").style.display = "none";
-  // var x = document.getElementById("insert-data");
-  // console.log("i m hide"  + x.style.display);
+  row.setAttribute("id", "row-" + idVAlue);
+  idCell.innerHTML = idVAlue;
+  fnameCell.innerHTML = personData.fname ? personData.fname : 'n/a';
+  lnameCell.innerHTML = personData.lname ? personData.lname : 'n/a';
+  ageCell.innerHTML = personData.age ? personData.age : 'n/a';
+  profileCell.innerHTML = personData.profile  ? personData.profile : 'n/a';
+
+  var editBtn = document.createElement("BUTTON");
+  editBtn.innerHTML = " EDIT ";
+  editBtn.setAttribute("class", "btn btn-success");
+  editBtn.setAttribute("id", "edit-"+idVAlue);
+  editBtn.setAttribute("data-value", idVAlue);
+  editBtn.addEventListener("click", function() {
+    var id = this.getAttribute("data-value");
+    getRowData(id);
+  });
+  actionCell.appendChild(editBtn);
+
+  personRecords.push(personData);
 
   rowNumber++;
-
-  // console.log(person.id);
-  // console.log(person.fname);
-  // console.log(person.lname);
-  // console.log(person.age);
-  // console.log(person.profile);
+   idVAlue += 1;
 }
+*/
