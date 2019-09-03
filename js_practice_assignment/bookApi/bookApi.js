e4094b66826bf32964c7075cdedfe369253dc974
prachi-dubey <prachi@newput.com>
2019-09-03 17:48:05 +0530

    responsive bookapi

--- js_practice_assignment/bookApi/bookApi.js
+++ js_practice_assignment/bookApi/bookApi.js
@@ -1,6 +1,10 @@
-$(document).ready(function(){
-   getBookDetail()
-   $('li').on('click', getSubName );
+$(document).ready(function() {
+  console.log("Your screen resolution is: " );
+  console.log(screen.width);
+  getScreenInfo(screen.width);
+
+  getBookDetail();
+  $('li').on('click', getSubName);
 });
 
 function getSubName() {
@@ -10,6 +14,10 @@ function getSubName() {
   clickSub = $(this).data('book');
   console.log(clickSub);
   $(this).addClass('highlight');
+
+  if(screen.width < 780){
+    $('#sidebar').addClass('hidden');
+  }
   getBookDetail(clickSub)
 }
 
@@ -18,7 +26,7 @@ function getBookDetail(clickSub) {
   if(clickSub) {
     $('#books-wrapper').empty();
   }
-  $('#loading').removeClass('hidden');
+  $('#loading-status').removeClass('hidden');
   var test = clickSub ? clickSub : 'java';
   var tmp = 'https://api.itbook.store/1.0/search/'+ test +''; console.log(tmp);
   $.get('https://api.itbook.store/1.0/search/'+ test +'', function(data, status) {
@@ -32,15 +40,20 @@ function getBookDetail(clickSub) {
 function booksContainer(data) {
   var template = $('#book-api').html();
   var output = Mustache.render(template, data);
-  $('#loading').addClass('hidden');
+  $('#loading-status').addClass('hidden');
   $('#books-wrapper').html(output);
 }
 
-function myFunction() {
-  var x = $("#myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
+function showSidebar() {
+   $('#sidebar').removeClass('hidden');
+}
+
+function getScreenInfo(screenWidth) {
+  if(screenWidth < 780) {                 //console.log("i am small device");
+    $('#humburger').removeClass('hidden');
+    $('#sidebar').addClass('hidden');
+  } else {                               //console.log("i am big device");
+    $('#humburger').addClass('hidden');
+    $('#sidebar').removeClass('hidden');
   }
 }
