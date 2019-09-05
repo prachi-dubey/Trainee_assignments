aecf9cdbd7b2a4a5215adb442c073be0bd53b054
prachi-dubey <prachi@newput.com>
2019-09-05 09:30:20 +0530

    book-api

--- js_practice_assignment/bookApi/bookApi.js
+++ js_practice_assignment/bookApi/bookApi.js
@@ -1,59 +1,100 @@
-$(document).ready(function() {
-  console.log("Your screen resolution is: " );
-  console.log(screen.width);
-  getScreenInfo(screen.width);
-
-  getBookDetail();
-  $('li').on('click', getSubName);
+$(window).resize(function() {
+  getScreenInfo();
 });
 
-function getSubName() {
-  $('.side-nav-left li').removeClass('highlight');
-
-  var clickSub = null;
-  clickSub = $(this).data('book');
-  console.log(clickSub);
-  $(this).addClass('highlight');
-
-  if(screen.width < 780){
-    $('#sidebar').addClass('hidden');
-  }
-  getBookDetail(clickSub)
-}
-
-function getBookDetail(clickSub) {
-  // console.log(clickSub);
-  if(clickSub) {
-    $('#books-wrapper').empty();
-  }
-  $('#loading-status').removeClass('hidden');
-  var test = clickSub ? clickSub : 'java';
-  var tmp = 'https://api.itbook.store/1.0/search/'+ test +''; console.log(tmp);
-  $.get('https://api.itbook.store/1.0/search/'+ test +'', function(data, status) {
-  console.log("data: ");
-  console.log(data);
-  // console.log("Status: " + status);
-  booksContainer(data);
-  });
-}
-
-function booksContainer(data) {
-  var template = $('#book-api').html();
-  var output = Mustache.render(template, data);
-  $('#loading-status').addClass('hidden');
-  $('#books-wrapper').html(output);
-}
+$(document).ready(function() {
+  getSubDetails();
+  $('#sidebar li').on( "click", getSubName);
+});
 
 function showSidebar() {
    $('#sidebar').removeClass('hidden');
 }
 
-function getScreenInfo(screenWidth) {
-  if(screenWidth < 780) {                 //console.log("i am small device");
-    $('#humburger').removeClass('hidden');
-    $('#sidebar').addClass('hidden');
-  } else {                               //console.log("i am big device");
-    $('#humburger').addClass('hidden');
-    $('#sidebar').removeClass('hidden');
+function getSubName() {
+  $('#sidebar li').removeClass('highlight');
+  $(this).addClass('highlight');
+  var subject = null;
+  subject = $(this).data('book');
+  // console.log(subject);
+  getSubDetails(subject);
+}
+
+function getSubDetails(subject) {
+  // console.log(subject);
+  if(subject) {
+    $('#books-wrapper').empty();
+  }
+  $('#loading-status').removeClass('hidden');
+  if(!subject) {
+    subject = 'java';
+  }
+  var url = 'https://api.itbook.store/1.0/search/' + subject;
+  console.log(url);
+  $.get(url, function(data, status) {
+    // console.log("data: ");
+    // console.log(data);
+    // console.log("Status: " + status);
+    booksContainer(data);
+  });
+}
+
+function booksContainer(data) {
+  var template = $('#book-template').html();
+  var formatData = parseData(data);
+  var output = Mustache.render(template, formatData);
+  $('#loading-status').addClass('hidden');
+  $('#books-wrapper').html(output);
+  getScreenInfo();
+}
+
+function getScreenInfo() {
+  var screenWidth = $(window).width();
+  if(screenWidth < 780) {
+    $('#humburger').toggleClass('hidden');
+    $('#sidebar').toggleClass('hidden');
   }
 }
+
+function shortData(text, size) {
+    var len = text.length;
+    if (len >= size) {
+        text = text.substring(0, size) + "...";
+    }
+    return text;
+};
+
+function parseData(data) {
+//   console.log(data);
+  var books  = data.books;
+  for(var i = 0; i < books.length; i++ ) {
+    var title = books[i].title;
+    books[i].title = shortData(title, 25);
+    var subtitle = books[i].subtitle;
+    books[i].subtitle = shortData(subtitle, 47);
+  }
+  console.log(data);
+  return data;
+}
+
+// function imgLoad(image) {
+//     // image.imgload = "";
+//     image.src = "download.png";
+//     return true;
+// }
+
+function init() {
+ var imgDefer = $('img');
+ console.log(imgDefer);
+  var test = $(imgDefer[i]).attr('data-src');
+  for (var i=0; i<imgDefer.length; i++) {
+    console.log("differ");
+    console.log(test);
+    if(test) {
+    $(imgDefer[i]).attr('src',test);
+    }
+  }
+}
+
+window.onload = init;
+// document.onload = init;
